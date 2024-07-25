f32bd79d86d6403e2a966bee7c5da15bc4b81d2c
bbognar <bbognar@ventus.group>
2024-07-25 13:08:19 +0200

    New API: - Create new User

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +28,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/user/", include("user.urls")),
 ]
