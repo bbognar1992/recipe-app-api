afe16b23755a1e9f2da3ed8fe079306e7f9a341b
bbognar <bbognar@ventus.group>
2024-08-12 10:33:48 +0200

    Add recipe detail APIs

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,7 @@
 """
 Serialzers for recipe APIs.
 """
+
 from rest_framework import serializers
 
 from core.models import Recipe
@@ -11,7 +12,12 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only_fields = ['id']
+        fields = ["id", "title", "time_minutes", "price", "link"]
+        read_only_fields = ["id"]
 
-        
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ["description"]
