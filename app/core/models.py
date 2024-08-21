b6eb51ebf7492789742dd2d3281d62730ca8347f
bbognar <bbognar@ventus.group>
2024-08-21 14:04:34 +0200

    Added tags model

--- app/core/models.py
+++ app/core/models.py
@@ -52,14 +52,26 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Recipe(models.Model):
     """Recipe object."""
 
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tag = models.ManyToManyField("Tag")
 
     def __str__(self) -> str:
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.name
