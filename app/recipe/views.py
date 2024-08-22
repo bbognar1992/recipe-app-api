484b3f1cdd92e27c74c0e0568a71a93a69970ba5
bbognar <bbognar@ventus.group>
2024-08-22 14:15:09 +0200

    fix flake8

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -33,8 +33,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
+
 class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """Manage tags in the database."""
+
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -43,4 +45,3 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by("-name")
-    
